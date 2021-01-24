cae717d295d0be6b9fe126d9fb19a03f8fdbafe0
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-01-24 21:05:13 +0300

    adding DataPicker functionality some refactoring

--- app/src/main/java/com/example/notes/SingleNoteFragment.java
+++ app/src/main/java/com/example/notes/SingleNoteFragment.java
@@ -1,17 +1,21 @@
 package com.example.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SingleNoteFragment extends Fragment {
 
     static final String ARG_SINGLE_NOTE = "note";
+    private static final int REQUEST_CODE = 99;
+    static final String DATE_EXTRA = "dateForPicker";
+
     private Note note;
 
     public SingleNoteFragment() {
@@ -41,16 +45,36 @@ public class SingleNoteFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_single_note, container, false);
         TextView tvName = view.findViewById(R.id.text_view_name);
         tvName.setText(note.getName());
-        EditText etDate = view.findViewById(R.id.edit_text_date);
+        TextView tvDate = view.findViewById(R.id.text_view_date);
+        tvDate.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), DataPickerActivity.class);
+
+            intent.putExtra(DATE_EXTRA, note.getCreationDate());
+            startActivityForResult(intent, REQUEST_CODE);
+        });
 //        String pattern = getResources().getString(R.string.data_format);
 //        DateFormat df = new SimpleDateFormat(pattern);
 //        String todayAsString = df.format(note.getCreationDate());
-        etDate.setText(note.getCreationDate());
+        tvDate.setText(note.getCreationDate());
         TextView tvDescription = view.findViewById(R.id.text_view_description);
         tvDescription.setText(note.getDescription());
         TextView tvContent = view.findViewById(R.id.edit_text_content);
         tvContent.setText(note.getContent());
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode != REQUEST_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if (resultCode == getActivity().RESULT_OK) {
+            String dateStr = data.getExtras().getString(DATE_EXTRA);
+            note.setCreationDate(dateStr);
+            TextView tvDate = getActivity().findViewById(R.id.text_view_date);
+            tvDate.setText(dateStr);
+        }
     }
 }
