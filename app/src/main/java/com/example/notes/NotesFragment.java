7e45aabc2ac07818e0ebfc030fb3b66722cd641a
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-01-27 22:25:31 +0300

    adding menu to NotesFragment

--- app/src/main/java/com/example/notes/NotesFragment.java
+++ app/src/main/java/com/example/notes/NotesFragment.java
@@ -5,10 +5,14 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -119,7 +123,28 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.add_note:
+                Toast.makeText(getContext(), "Add chosen", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.sort_notes:
+                Toast.makeText(getContext(), "Sort chosen", Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
