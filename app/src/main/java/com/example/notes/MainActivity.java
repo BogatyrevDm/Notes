2c63969aebd5fea55d411149ec6c788e4f6ad1dd
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-01-28 18:22:38 +0300

    adding drawer header

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_bar_main);
         initToolBar();
         if (savedInstanceState == null) {
             NotesFragment details = new NotesFragment();
